75524a501d6a4b3dfcbca1f5365d3f4f49b22733
Adam Pavlidis <adampavlidis@gmail.com>
2017-06-24 01:48:26 +0300

    Ground works for automating new-vm vars. Trivial addition to play execution

--- new_vm_wrapper.py
+++ new_vm_wrapper.py
@@ -1,4 +1,41 @@
 #!/usr/bin/env python
 
-# FIND FREE IP
+import json
 
+
+def main():
+    # Variables for Packages (base_packages, extra_packages, UPGRADE_DIST_FLAG)
+
+    # json.loads default values
+    defaults = dict()
+    defaults['main_key_path'] = '~/.ssh/id_rsa.pub'
+    defaults['exempt_addresses'] = list()
+
+    # main_key_path,
+    # exempt addresses
+
+    # network stuff
+    defaults['netmask'] = '255.255.255.0'
+
+    vars = {
+        "extra_packages": "",
+        "UPGRADE_DIST_FLAG": True,
+        "main_key_path": "~/.ssh/id_rsa.pub",
+        "primary_ip": "",
+        "primary_netmask": "",
+        "primary_network": "",
+        "primary_broadcast": "",
+        "primary_dns1": "",
+        "primary_dns2": "",
+        "gateway": "",
+        "privileged_host": [{"addr": "", "id": 1, "state": "present"},
+                            {"addr": "", "id": 2, "state": "present"}]
+    }
+
+    with open('/tmp/vars.json', 'w') as f:
+        json.dump(vars, f)
+    # FIND FREE IP
+    return 0
+
+if __name__ == '__main__':
+    main()
