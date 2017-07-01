5e836cf1858c70ff3450f16edd35a0b0a80f6b3a
Adam Pavlidis <adampavlidis@gmail.com>
2017-07-01 20:49:28 +0300

    Minor Bugfixes + implemented TODO for privileged_host processing

--- new_vm_wrapper.py
+++ new_vm_wrapper.py
@@ -6,7 +6,10 @@ import sys
 import ipaddress
 from pathlib import Path
 
-DEFAULTS_PATH = '/tmp/defaults.json'
+DEFAULTS_PATH = 'new_vm_defaults.json'
+
+ip_args = ["primary_ip", "primary_netmask", "primary_network",
+           "primary_broadcast", "primary_dns1", "primary_dns2", "gateway"]
 
 
 def query_yes_no(question, default="yes"):
@@ -50,6 +53,40 @@ def read_ip():
     pass
 
 
+def read_priviliged_hosts(priv_len=1):
+    func_priv_args = []
+    while True:
+        priv_dict = dict()
+        ip_addr = None
+        while not ip_addr:
+            read_host = input("Enter an IP Address for Privileged Host : >> ")
+            try:
+                ip_addr = ipaddress.ip_address(read_host)
+            except ValueError:
+                if read_host == "":
+                    break
+                print("Bad IP address!")
+
+        if not read_host:
+            break
+
+        read_state = query_yes_no("Host Present?")
+
+        priv_dict["addr"] = ip_addr
+        priv_dict["id"] = priv_len
+
+        if read_state:
+            priv_dict["state"] = "present"
+        else:
+            priv_dict["state"] = "absent"
+
+        priv_len += 1
+
+        func_priv_args.append(priv_dict)
+
+    return func_priv_args
+
+
 def main():
     # Variables for Packages (base_packages, extra_packages, UPGRADE_DIST_FLAG)
 
@@ -60,15 +97,12 @@ def main():
     except IOError:
         print("Skipping loaded variables, No such FILE: %s" % DEFAULTS_PATH)
         print("You have to provide all variables")
-        def_vars = None
+        def_vars = dict()
 
     if def_vars:
         print(def_vars)
         for k, v in def_vars.items():
             print(k, v)
-
-    ip_args = ["primary_ip", "primary_netmask", "primary_network",
-               "primary_broadcast", "primary_dns1", "primary_dns2", "gateway"]
 
     # Validate Upgrade Dist
     # "UPGRADE_DIST_FLAG"
@@ -78,7 +112,7 @@ def main():
 
     # Validate Key Path
     if "main_key_path" not in def_vars or not Path(os.path.expanduser(def_vars["main_key_path"])).is_file():
-        print("default \"main_key_path\" points to an invalid file. Ignoring\n")
+        print("default \"main_key_path\" points to an invalid file or does not exist. Ignoring\n")
         main_key_path = None
     else:
         print("Default value for \"main_key_path\" points to %s.\n" % def_vars["main_key_path"])
@@ -91,6 +125,7 @@ def main():
 
     print(main_key_path)
 
+    # Validate IP Settings
     for ip_arg in ip_args:
         if ip_arg in def_vars:
             print("\nDefault Value for \"%s\" is:\t %s\n" % (ip_arg, def_vars[ip_arg]))
@@ -112,17 +147,30 @@ def main():
 
     # TODO
     priv_args = []
+    if "privileged_host" in def_vars:
+        priv_args = def_vars["privileged_host"]
+
+        # TODO CHECK THE PRIV_HOSTS
+
+        # TODO ASK IF YOU WANT MORE
+        for elem in read_priviliged_hosts(priv_len=len(priv_args)):
+            priv_args.append(elem)
+    else:
+        print("\nNo privileged host. Please enter IPs. Or enter empty string to stop\n")
+        for elem in read_priviliged_hosts():
+            priv_args.append(elem)
+
+    vm_vars["privileged_host"] = priv_args
 
     # TODO
     extra_pack = []
-    # main_key_path,3no
     # exempt addresses
 
-    # network stuff
+    vm_vars["extra_packages"] = extra_pack
 
-    print(vm_vars)
+    print(json.dumps(vm_vars, indent=4))
 
-    with open('/tmp/vars.json', 'w') as f:
+    with open('new_vm_vars.json', 'w') as f:
         json.dump(vm_vars, f)
     # FIND FREE IP
     return 0
