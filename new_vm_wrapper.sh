2b444bd2831f3161758bc22e41a2a1bee697a85e
Adam Pavlidis <adampavlidis@gmail.com>
2017-07-02 17:30:27 +0300

    Minor changes.

--- new_vm_wrapper.sh
+++ new_vm_wrapper.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-python new_vm_var_populate.py
+# python new_vm_var_populate.py
+
+# INPUT Playbook to run
+
+# INPUT enter username
+
+# INPUT sudo (-K)?
+
+# INPUT password (-k)?
+
+# INPUT JSON FILE
+
+# INPUT target host
+
+echo "ansible-playbook $1 -i '$2', -kK -u $3 -e @$4"
+
+# TODO Possibly Pythonize it ?
+
+ansible-playbook $1 -i '$2', -kK -u $3 -e @$4
