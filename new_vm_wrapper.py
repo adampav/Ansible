6210f4f9d7b3fd872b534f884005890eda1bd5a3
Adam Pavlidis <adampavlidis@gmail.com>
2017-07-02 14:21:18 +0300

    Finished Priviliged_Host processing

--- new_vm_wrapper.py
+++ new_vm_wrapper.py
@@ -12,10 +12,126 @@ ip_args = ["primary_ip", "primary_netmask", "primary_network",
            "primary_broadcast", "primary_dns1", "primary_dns2", "gateway"]
 
 
+class HostExemption:
+    def __init__(self, **kwargs):
+        validator = self.validate(**kwargs)
+
+    @staticmethod
+    def validate(raise_f=True, **kwargs):
+        needed_args = ["id", "state", "addr"]
+        acceptable = {
+            "state": ["absent", "present"]
+        }
+
+        for arg in needed_args:
+            if arg not in kwargs or not kwargs[arg]:
+                if raise_f:
+                    raise ValueError
+                else:
+                    return False
+
+        if not isinstance(kwargs["id"], int):
+            if raise_f:
+                raise TypeError("id is not an Integer")
+            else:
+                return False
+
+        if kwargs["state"] != "present" and kwargs["state"] != "absent":
+            print(json.dumps(acceptable["state"], indent=4))
+            if raise_f:
+                raise ValueError("Invalid state value. Select one from the list.")
+            else:
+                return False
+
+        try:
+            ipaddress.ip_address(kwargs["addr"])
+        except ValueError:
+            if raise_f:
+                ipaddress.ip_address(kwargs["addr"])
+            else:
+                return False
+
+        return True
+
+    @staticmethod
+    def read_priviliged_hosts(priv_len=1):
+        func_priv_args = []
+        while True:
+            priv_dict = dict()
+            ip_addr = None
+            read_host = ""
+            while not ip_addr:
+                read_host = input("Enter an IP Address for Privileged Host : >> ")
+                try:
+                    ip_addr = ipaddress.ip_address(read_host)
+                except ValueError:
+                    if read_host == "":
+                        break
+                    print("Bad IP address!")
+
+            if not read_host:
+                break
+
+            read_state = query_yes_no("Host Present?")
+
+            priv_dict["addr"] = str(ip_addr)
+            priv_dict["id"] = priv_len
+
+            if read_state:
+                priv_dict["state"] = "present"
+            else:
+                priv_dict["state"] = "absent"
+
+            priv_len += 1
+
+            func_priv_args.append(priv_dict)
+
+        return func_priv_args
+
+
+class ExtraPackages:
+    def __init__(self, **kwargs):
+        validator = self.validate(**kwargs)
+
+    @staticmethod
+    def validate(raise_f=True, **kwargs):
+        needed_args = ["name", "state"]
+        acceptable = {
+            "state": ["absent", "present", "latest"],
+            "name": ["vim", "git", "nmap", "tcpdump", "iptables", "iptables-persistent", "ufw"]
+        }
+
+        for arg in needed_args:
+            if arg not in kwargs or not kwargs[arg]:
+                if raise_f:
+                    raise ValueError
+                else:
+                    return False
+
+        if kwargs["name"] not in acceptable["name"]:
+            print(json.dumps(acceptable["name"], indent=4))
+            if raise_f:
+                raise ValueError("Invalid Package Name. Select one from the list.")
+            else:
+                return False
+
+        if kwargs["state"] not in acceptable["state"]:
+            print(json.dumps(acceptable["state"], indent=4))
+            if raise_f:
+                raise ValueError("Invalid state value. Select one from the list.")
+            else:
+                return False
+
+        return True
+
+    @staticmethod
+    def read_extra_packages():
+        return []
+
+
 def query_yes_no(question, default="yes"):
     # Available from https://stackoverflow.com/questions/3041986/apt-command-line-interface-like-yes-no-input
     """
-
     :param question: String to be asked as a question
     :param default: Default answer to the question
     :return: True or False to the question
@@ -53,38 +169,12 @@ def read_ip():
     pass
 
 
-def read_priviliged_hosts(priv_len=1):
-    func_priv_args = []
-    while True:
-        priv_dict = dict()
-        ip_addr = None
-        while not ip_addr:
-            read_host = input("Enter an IP Address for Privileged Host : >> ")
-            try:
-                ip_addr = ipaddress.ip_address(read_host)
-            except ValueError:
-                if read_host == "":
-                    break
-                print("Bad IP address!")
+def read_pub_key():
+    main_key_path = None
+    while not main_key_path or not Path(os.path.expanduser(main_key_path)).is_file():
+        main_key_path = input("Enter a file path for main_key: >> ")
 
-        if not read_host:
-            break
-
-        read_state = query_yes_no("Host Present?")
-
-        priv_dict["addr"] = ip_addr
-        priv_dict["id"] = priv_len
-
-        if read_state:
-            priv_dict["state"] = "present"
-        else:
-            priv_dict["state"] = "absent"
-
-        priv_len += 1
-
-        func_priv_args.append(priv_dict)
-
-    return func_priv_args
+    return main_key_path
 
 
 def main():
@@ -99,31 +189,29 @@ def main():
         print("You have to provide all variables")
         def_vars = dict()
 
-    if def_vars:
-        print(def_vars)
-        for k, v in def_vars.items():
-            print(k, v)
+    print(json.dumps(def_vars, indent=4))
 
     # Validate Upgrade Dist
     # "UPGRADE_DIST_FLAG"
     vm_vars = dict()
 
-    UPGRADE_DIST_FLAG = query_yes_no("Do you want to update/upgrade the system?")
+    vm_vars["UPGRADE_DIST_FLAG"] = query_yes_no("Do you want to update/upgrade the system?")
 
     # Validate Key Path
     if "main_key_path" not in def_vars or not Path(os.path.expanduser(def_vars["main_key_path"])).is_file():
         print("default \"main_key_path\" points to an invalid file or does not exist. Ignoring\n")
-        main_key_path = None
+
+        main_key_path = read_pub_key()
+
     else:
         print("Default value for \"main_key_path\" points to %s.\n" % def_vars["main_key_path"])
         if query_yes_no("Keep the default value?"):
             main_key_path = def_vars["main_key_path"]
         else:
-            main_key_path = None
-            while not main_key_path or not Path(os.path.expanduser(main_key_path)).is_file():
-                main_key_path = input("Enter a file path for main_key: >> ")
+            main_key_path = read_pub_key()
 
     print(main_key_path)
+    vm_vars["main_key_path"] = main_key_path
 
     # Validate IP Settings
     for ip_arg in ip_args:
@@ -148,34 +236,52 @@ def main():
     # TODO
     priv_args = []
     if "privileged_host" in def_vars:
+        # VALIDATE Already Present vars
+        def_vars["privileged_host"] = [elem for elem in def_vars["privileged_host"]
+                                       if HostExemption.validate(raise_f=False, **elem)]
 
-        for elem in def_vars["privileged_host"]:
-            pass
-            # TODO create 2 classes for priv_host + extra_vars
+        priv_args = [elem for elem in def_vars["privileged_host"]
+                     if query_yes_no(json.dumps(elem, indent=4)+"\nKeep this?\n")]
 
+        if query_yes_no("Do you want to insert more?"):
+            print("\nNo privileged host. Please enter IPs. Or enter empty string to stop\n")
+            for elem in HostExemption.read_priviliged_hosts(priv_len=len(priv_args)):
+                priv_args.append(elem)
 
-
-        priv_args = def_vars["privileged_host"]
-        # TODO ASK IF YOU WANT MORE
-        for elem in read_priviliged_hosts(priv_len=len(priv_args)):
-            priv_args.append(elem)
     else:
         print("\nNo privileged host. Please enter IPs. Or enter empty string to stop\n")
-        for elem in read_priviliged_hosts():
+        for elem in HostExemption.read_priviliged_hosts():
             priv_args.append(elem)
 
     vm_vars["privileged_host"] = priv_args
 
     # TODO
     extra_pack = []
-    # exempt addresses
+
+    if "extra_packages" in def_vars:
+        # VALIDATE Already Present vars
+        def_vars["extra_packages"] = [elem for elem in def_vars["extra_packages"]
+                                      if ExtraPackages.validate(raise_f=False, **elem)]
+
+        priv_args = [elem for elem in def_vars["extra_packages"]
+                     if query_yes_no(json.dumps(elem, indent=4)+"\nKeep this?\n")]
+
+        if query_yes_no("Do you want to insert more?"):
+            print("\nEnter \n")
+            for elem in ExtraPackages.read_extra_packages():
+                priv_args.append(elem)
+
+    else:
+        print("\nNo privileged host. Please enter IPs. Or enter empty string to stop\n")
+        for elem in ExtraPackages.read_extra_packages():
+            priv_args.append(elem)
 
     vm_vars["extra_packages"] = extra_pack
 
     print(json.dumps(vm_vars, indent=4))
 
     with open('new_vm_vars.json', 'w') as f:
-        json.dump(vm_vars, f)
+        json.dump(vm_vars, f, indent=4)
     # FIND FREE IP
     return 0
 
